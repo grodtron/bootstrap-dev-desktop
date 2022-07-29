e6ba1fa1d74a9317409e78238e78786f44a69c10
Gordon Bailey <gb@gordonbailey.net>
2022-07-29 07:58:27 +0000

    Improvement after second test
    Forgot to disable showing untracked files in dotfiles

--- user/10-install-dotfiles.sh
+++ user/10-install-dotfiles.sh
@@ -8,8 +8,10 @@ cd $HOME
 
 git clone --bare https://github.com/grodtron/dotfiles.git $HOME/$DOTFILES_GIT_REPO_DIR_NAME
 
-git --git-dir "$HOME/$DOTFILES_GIT_REPO_DIR_NAME" --work-tree "$HOME" checkout
-git --git-dir "$HOME/$DOTFILES_GIT_REPO_DIR_NAME" --work-tree "$HOME" submodule init
-git --git-dir "$HOME/$DOTFILES_GIT_REPO_DIR_NAME" --work-tree "$HOME" submodule update
+alias dotfile-git="git --git-dir '$HOME/$DOTFILES_GIT_REPO_DIR_NAME' --work-tree '$HOME'"
 
+dotfile-git checkout
+dotfile-git submodule init
+dotfile-git submodule update
+dotfile-git config status.showUntrackedFiles no
 
