c549c9a9258c1db1a8a791350f36de01b2db4530
Gordon Bailey <gb@gordonbailey.net>
2022-07-29 08:28:52 +0000

    Install personal public key

--- user/06-install-ssh-certs.sh
+++ user/06-install-ssh-certs.sh
@@ -5,3 +5,5 @@ set -eu
 aws s3 cp s3://grodtron-private-keys/github-2022-07-27 $HOME/.ssh/github
 
 chmod go-rw $HOME/.ssh/github
+
+aws s3 cp s3://grodtron-public-keys/personal_id_ed25519.pub - > $HOME/.ssh/authorized_keys
