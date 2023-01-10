b6cb1d8ffb17b38eb801d7ba84f9755e9870dfa5
Ubuntu <ubuntu@ip-172-31-35-189.eu-central-1.compute.internal>
2023-01-10 08:13:33 +0000

    Fix bug in git configure script

--- user/08-configure-git.sh
+++ user/08-configure-git.sh
@@ -2,5 +2,8 @@
 
 set -eu
 
+# For some reason this is necessary for the following commands to work. 
+cd $HOME
+
 git config --global user.name "Gordon Bailey"
 git config --global user.email "github@gordonbailey.net"
