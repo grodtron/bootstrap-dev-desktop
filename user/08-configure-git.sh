5ca83e93abf1e56dc0bcc8a50e67c6eb7502dcec
Gordon Bailey <github@gordonbailey.net>
2023-01-11 01:30:13 +0000

    Revert "Fix bug in git configure script"
    This reverts commit b6cb1d8ffb17b38eb801d7ba84f9755e9870dfa5.

--- user/08-configure-git.sh
+++ user/08-configure-git.sh
@@ -2,8 +2,5 @@
 
 set -eu
 
-# For some reason this is necessary for the following commands to work. 
-cd $HOME
-
 git config --global user.name "Gordon Bailey"
 git config --global user.email "github@gordonbailey.net"
