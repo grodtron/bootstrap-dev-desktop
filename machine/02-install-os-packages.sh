42ce56c8a369a57c091b4ae819df30420262bd4a
Gordon Bailey <github@gordonbailey.net>
2023-08-02 23:11:51 +0000

    Add terraform to os-packages

--- machine/02-install-os-packages.sh
+++ machine/02-install-os-packages.sh
@@ -20,6 +20,23 @@ echo "deb [signed-by=/etc/keys/llvm-snapshot.gpg] http://apt.llvm.org/jammy/ llv
    sudo tee --append /etc/apt/sources.list
 
 #########
+# Add deb for terraform
+
+# Annoying that the commands here are so different from those above for LLVM,
+# but not worth messing with, if it works, it works.
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+    gpg --dearmor | \
+    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg | file -
+
+gpg --no-default-keyring \
+    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+    --fingerprint
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+#########
 
 sudo apt-get update -y
 
@@ -33,5 +50,6 @@ sudo apt-get install -y \
     awscli \
     jq \
     zsh \
+    terraform \
 
 
