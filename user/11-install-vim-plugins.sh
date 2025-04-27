ea90fe377cc751a4a9334385c4eea629ffd3119d
Gordon Bailey <github@gordonbailey.net>
2025-04-27 00:42:55 +0000

    Rewrite vim plugin install
    The old method never really worked. Now we just brute force it by
    parsing through .vimrc and manually cloning the plugins

--- user/11-install-vim-plugins.sh
+++ user/11-install-vim-plugins.sh
@@ -2,4 +2,11 @@
 
 set -eu
 
-vim -es -c ':PluginInstall' -c ':qall'
+for plugin in $(grep '^Plugin' ~/.vimrc | awk '{print $2}' | tr -d "'")
+do
+    plugin_name=$(basename $plugin)
+    url=https://github.com/$plugin
+    dest=~/.vim/bundle/$plugin_name
+
+    git clone $url $dest
+done
