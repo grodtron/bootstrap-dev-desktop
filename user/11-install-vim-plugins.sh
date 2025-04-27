6f213d82a186fd05366a8129b51d33dc5e3c27d3
Gordon Bailey <github@gordonbailey.net>
2025-04-27 02:01:35 +0000

    Allow each vim plugin install to fail

--- user/11-install-vim-plugins.sh
+++ user/11-install-vim-plugins.sh
@@ -2,11 +2,15 @@
 
 set -eu
 
+PLUGIN_ROOT=~/.vim/bundle
+
+mkdir -p $PLUGIN_ROOT
+
 for plugin in $(grep '^Plugin' ~/.vimrc | awk '{print $2}' | tr -d "'")
 do
     plugin_name=$(basename $plugin)
     url=https://github.com/$plugin
-    dest=~/.vim/bundle/$plugin_name
+    dest=$PLUGIN_ROOT/$plugin_name
 
-    git clone $url $dest
+    git clone $url $dest || echo "Skipping $plugin"
 done
