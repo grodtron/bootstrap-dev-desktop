c7e65de6a98fee86ecac9c9e1954b2608ad2dce9
Gordon Bailey <grodtron@users.noreply.github.com>
2025-05-12 23:53:05 +0200

    Do chmod +x on each file before executing

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,10 +6,12 @@ source env.sh
 
 for file in $(ls -1 machine/)
 do
+    chmod +x ./machine/$file
     ./machine/$file 2>&1 | tee ${file%.*}.log
 done
 
 for file in $(ls -1 user/)
 do
+    chmod +x ./user/$file
     sudo -u gordon -g gordon --preserve-env=DOTFILES_GIT_REPO_DIR_NAME ./user/$file 2>&1 | tee ${file%.*}.log
 done
