e90765ef40319ac894edbac763c78c1f3a9e4bea
Gordon Bailey <github@gordonbailey.net>
2025-05-30 00:51:14 +0200

    Ensure that we don't run tmux when sourcing .zshrc

--- user/12-install-node-npm.sh
+++ user/12-install-node-npm.sh
@@ -2,6 +2,8 @@
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | zsh
 
+# Without this, re-sourcing zshrc will try to open tmux
+export TMUX='dont-open-tmux'
 source ~/.zshrc
 
 nvm install --lts 
